2be1bc7de7c5081b5682dd58e450e5cbbf1ee78b
mardony01 <mardonysilva04@gmail.com>
2025-02-12 21:09:59 -0300

    Atualizando a classe Mesa

--- src/domain/simulation-engine/system/mesa.ts
+++ src/domain/simulation-engine/system/mesa.ts
@@ -6,34 +6,32 @@ import { Aluno } from "./aluno";
  */
 class Table {
     /** Lista de alunos atualmente na mesa */
-    alunoNaMesa: Aluno[];
-    
+    private alunoNaMesa: Aluno[];
+
     /** Número máximo de alunos que podem ocupar a mesa */
-    quantidadeMaxima: number;
-    
+    private quantidadeMaxima: number;
+
     /** Número de mesas ocupadas no momento */
-    mesaOcupada: number;
-    
+    private mesaOcupada: number;
+
     /** Tempo máximo que um aluno pode permanecer na mesa (em minutos) */
     tempoDePermanencia: number;
 
     /**
      * Construtor da classe Table.
-     * @param numeroDeMesa - Número máximo de alunos permitidos na mesa.
-     * @param mesasOcupadas - Número de mesas já ocupadas.
-     * @param tempoDePermanencia - Tempo máximo de permanência dos alunos na mesa.
+     * Inicializa uma nova mesa sem alunos, com quantidade máxima, mesas ocupadas e tempo de permanência zerados.
      */
     constructor() {
         this.alunoNaMesa = [];
         this.quantidadeMaxima = 0;
-        this.mesaOcupada = 0
+        this.mesaOcupada = 0;
         this.tempoDePermanencia = 0;
     }
 
     /**
-     * Verifica se um aluno está na mesa.
+     * Verifica se um aluno está presente na mesa.
      * @param aluno - O aluno a ser verificado.
-     * @returns O aluno encontrado ou undefined se ele não estiver na mesa.
+     * @returns O aluno encontrado ou undefined caso ele não esteja na mesa.
      */
     verificarAlunoNaMesa(aluno: Aluno): Aluno | undefined {
         for (let i = 0; i < this.alunoNaMesa.length; i++) {
@@ -45,44 +43,72 @@ class Table {
     }
 
     /**
-     * Adiciona um aluno à mesa, se ele ainda não estiver presente.
+     * Adiciona um aluno à mesa, se houver espaço disponível e ele ainda não estiver presente.
      * @param aluno - O aluno que deseja ocupar a mesa.
-     * @throws Erro caso o aluno já esteja na mesa.
+     * @throws Erro caso todas as mesas estejam ocupadas ou o aluno já esteja na mesa.
      */
-    ocuparMesa(aluno : Aluno){
-        if(this.mesaOcupada==this.quantidadeMaxima){
-            throw Error('Todas as mesas estão ocupadas.')
-        }else{
-            if(this.verificarAlunoNaMesa(aluno) == undefined){
-                this.alunoNaMesa.push(aluno);
-                this.mesaOcupada++
-            }else{throw Error("O aluno já está na mesa.")}
+    ocuparMesa(aluno: Aluno | undefined) {
+        if (this.mesaOcupada == this.quantidadeMaxima) {
+            throw Error('Todas as mesas estão ocupadas.');
+        } else {
+            if (this.verificarAlunoNaMesa(aluno!) == undefined) {
+                this.alunoNaMesa.push(aluno!);
+                this.mesaOcupada++;
+            } else {
+                throw Error('O aluno já está na mesa.');
+            }
         }
     }
 
     /**
-     * Remove um aluno da mesa, caso ele esteja presente.
-     * @param aluno - O aluno que deseja sair da mesa.
-     * @throws Erro caso o aluno não esteja na mesa.
+     * Descobre a posição do aluno com menor tempo de permanência na mesa.
+     * @returns A posição do aluno com menor tempo de permanência ou null caso a mesa esteja vazia.
      */
-    liberarMesa(aluno: Aluno) {
-        if (this.verificarAlunoNaMesa(aluno) !== undefined) {
-            for (let i = 0; i < this.alunoNaMesa.length; i++) {
-                if (this.alunoNaMesa[i] === aluno) {
-                    this.alunoNaMesa.splice(i, 1);
-                    this.mesaOcupada--
-                }
-            }
-        } else {
-            throw new Error('O aluno não está na mesa.');
+    descobrirOLigeirinho(): number | null {
+        if (this.alunoNaMesa.length === 0) {
+            return null;
         }
+
+        let posicaoDoLigeirinho: number = 0; // Índice do primeiro aluno
+        for (let posicaoAtual = 1; posicaoAtual < this.alunoNaMesa.length; posicaoAtual++) {
+            if (this.alunoNaMesa[posicaoAtual].getTempoPermanencia()! < this.alunoNaMesa[posicaoDoLigeirinho].getTempoPermanencia()!) {
+                posicaoDoLigeirinho = posicaoAtual;
+            }
+        }
+        return posicaoDoLigeirinho;
     }
 
-    setTMPNM(tMPNM:number){;
+    /**
+     * Remove um aluno da mesa, dando preferência ao que permaneceu por menos tempo.
+     * @throws Erro caso não haja alunos na mesa ou ocorra um erro ao tentar remover.
+     */
+    liberarMesa() {
+        if (this.alunoNaMesa.length == 0) {
+            throw Error('Não há alunos na mesa.');
+        }
+
+        let alunoQueVaiSair: number | null = this.descobrirOLigeirinho();
+        if (alunoQueVaiSair == null || this.alunoNaMesa[alunoQueVaiSair] == null) {
+            throw Error('Erro ao remover aluno: referência inválida.');
+        }
+
+        this.alunoNaMesa.splice(alunoQueVaiSair, 1);
+        this.mesaOcupada--;
+    }
+
+    /**
+     * Define o tempo máximo de permanência dos alunos na mesa.
+     * @param tMPNM - Tempo máximo de permanência (em minutos).
+     */
+    setTMPNM(tMPNM: number) {
         this.tempoDePermanencia = tMPNM;
     }
 
-    setLM(lM:number){
-        this.quantidadeMaxima = lM
+    /**
+     * Define a quantidade máxima de alunos que podem ocupar a mesa.
+     * @param lM - Número máximo de alunos permitidos.
+     */
+    setLM(lM: number) {
+        this.quantidadeMaxima = lM;
     }
 };
